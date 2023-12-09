4b5336127a97010393a8c562d758cafa6d390e5b
charitreddy <charitreddy@gmail.com>
2023-12-09 02:16:18 -0600

    Dec 8

--- buyAndSellStock.ts
+++
@@ -1,9 +0,0 @@
-function maxProfit(prices: number[]): number {
-  let max: number = -Infinity;
-  let profit: number = 0;
-  for (let i = prices.length - 1; i >= 0; i--) {
-    max = Math.max(max, prices[i]);
-    profit = Math.max(profit, max - prices[i]);
-  }
-  return profit;
-}
--- isIsomorphic.ts
+++
@@ -1,9 +0,0 @@
-function isIsomorphic(s: string, t: string): boolean {
-  for (let i = 0; i < s.length; i++) {
-    if (s.indexOf(s[i], i + 1) !== t.indexOf(t[i], i + 1)) return false;
-  }
-  return true;
-}
-
-//isIsomorphic("egg","add"); true
-//isIsomorphic("foo","bar"); false
